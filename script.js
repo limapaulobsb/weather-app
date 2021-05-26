f4f5abf301757b716650b4c6bfe54d109af1dfe6
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-05-26 10:54:19 -0300

    Add some CSS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const API_KEY = 'd3d6d3e42626a9197b7d1fd4072ddd88';
 
-async function getWeatherData(searchTerm) {
+async function fetchWeatherData(cityName) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=${cityName}`
     );
     return await response.json();
   } catch (error) {
@@ -15,7 +15,7 @@ const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 
 searchButton.addEventListener('click', async () => {
-  const searchTerm = searchInput.value;
-  const data = await getWeatherData(searchTerm);
+  const cityName = searchInput.value;
+  const data = await fetchWeatherData(cityName);
   console.log(data);
 });
