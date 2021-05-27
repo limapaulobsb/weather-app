42de3516dacc235cd4598bde4be32803dd8af102
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-05-27 09:17:39 -0300

    Add fetch function for forecast data

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const API_KEY = 'd3d6d3e42626a9197b7d1fd4072ddd88';
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
-async function fetchWeatherData(searchTerm, units) {
+let units, tempSymbol, speedUnit;
+
+function setUnits() {
+  if (document.getElementById('celcius').checked) {
+    units = 'metric';
+    tempSymbol = '&deg;C';
+    speedUnit = 'm/s';
+  } else {
+    units = 'imperial';
+    tempSymbol = '&deg;F';
+    speedUnit = 'mph';
+  }
+}
+
+async function fetchCurrentWeatherData(searchTerm, units) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=${units}&q=${searchTerm}`
@@ -13,31 +27,31 @@ async function fetchWeatherData(searchTerm, units) {
   }
 }
 
-async function showData() {
-  const searchTerm = searchInput.value;
-  let units, tempSymbol, speedUnit;
-
-  if (document.getElementById('celcius').checked) {
-    units = 'metric';
-    tempSymbol = '&deg;C';
-    speedUnit = 'm/s';
-  } else {
-    units = 'imperial';
-    tempSymbol = '&deg;F';
-    speedUnit = 'mph';
+async function fetchWeatherForecastData(searchTerm, units) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=${units}&q=${searchTerm}`
+    );
+    return await response.json();
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  const data = await fetchWeatherData(searchTerm, units);
+async function showCurrentData() {
+  const searchTerm = searchInput.value;
+  const currentData = await fetchCurrentWeatherData(searchTerm, units);
 
   const weatherDataElem = document.getElementById('weather-data');
+
   weatherDataElem.style.visibility = 'visible';
 
-  const cityName = data.name;
-  const countryCode = data.sys.country;
-  const temperature = data.main.temp;
-  const weatherDesc = data.weather[0].description;
-  const humidity = data.main.humidity;
-  const windSpeed = data.wind.speed;
+  const cityName = currentData.name;
+  const countryCode = currentData.sys.country;
+  const temperature = currentData.main.temp;
+  const weatherDesc = currentData.weather[0].description;
+  const humidity = currentData.main.humidity;
+  const windSpeed = currentData.wind.speed;
 
   const cityNameElem = document.getElementById('city-name');
   const countryFlagImg = document.getElementById('country-flag');
@@ -56,27 +70,25 @@ async function showData() {
   humidityElem.innerText = `Humidity levels at: ${humidity}%`;
   windSpeedElem.innerText = `Winds at: ${Math.round(windSpeed)} ${speedUnit}`;
 
-  console.log(data);
-  console.log(data.weather[0].main);
-  console.log(weatherDesc);
-  console.log(temperature);
-  console.log(data.main.feels_like);
-  console.log(data.main.temp_min);
-  console.log(data.main.temp_max);
-  console.log(humidity);
-  console.log(windSpeed);
-  console.log(data.clouds.all);
-  console.log(data.sys.country);
-  console.log(data.sys.sunrise);
-  console.log(data.sys.sunset);
-  console.log(data.timezone);
-  console.log(cityName);
+  console.log(currentData);
 }
 
-searchButton.addEventListener('click', () => showData());
+async function showForecastData() {
+  const searchTerm = searchInput.value;
+  const forecastData = await fetchWeatherForecastData(searchTerm, units);
+  console.log(forecastData);
+}
+
+searchButton.addEventListener('click', () => {
+  setUnits();
+  showCurrentData();
+  showForecastData();
+});
 
 searchInput.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
-    showData();
+    setUnits();
+    showCurrentData();
+    showForecastData();
   }
 });
