bda9c62b0d6c7e6eceb179d8ffcdfcea30242740
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-05-29 18:29:43 -0300

    Code improvements

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-import { DateTime } from './lib/luxon.min.js';
+import { Settings, DateTime } from './lib/luxon.min.js';
 
 const API_KEY = 'd3d6d3e42626a9197b7d1fd4072ddd88';
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
 
 let units, tempSymbol, speedUnit;
 
@@ -40,37 +38,65 @@ async function fetchWeatherForecastData(searchTerm, units) {
   }
 }
 
+function returnDateTime(time, offset, format) {
+  let timeZoneOffset = offset / 3600;
+  if (timeZoneOffset > 0) timeZoneOffset = `+${timeZoneOffset}`;
+  Settings.defaultZoneName = `UTC${timeZoneOffset}`;
+  let date;
+  if (isNaN(time)) {
+    date = DateTime.now();
+  } else {
+    date = DateTime.fromSeconds(time);
+  }
+  if (format) return date.toLocaleString(DateTime[format]);
+  return date;
+}
+
+const searchInput = document.getElementById('search-input');
+
+const searchButton = document.getElementById('search-button');
+
 async function showCurrentData() {
   const searchTerm = searchInput.value;
   const currentData = await fetchCurrentWeatherData(searchTerm, units);
 
   const weatherDataElem = document.getElementById('weather-data');
-
   weatherDataElem.style.visibility = 'visible';
 
-  const cityName = currentData.name;
   const countryCode = currentData.sys.country;
+  const cityName = currentData.name;
+
+  const localDateTime = returnDateTime(
+    'now',
+    currentData.timezone,
+    'DATETIME_MED'
+  );
+
   const temperature = currentData.main.temp;
   const weatherDesc = currentData.weather[0].description;
   const humidity = currentData.main.humidity;
   const windSpeed = currentData.wind.speed;
 
-  const cityNameElem = document.getElementById('city-name');
   const countryFlagImg = document.getElementById('country-flag');
+  const cityNameElem = document.getElementById('city-name');
+  const localDateTimeElem = document.getElementById('local-datetime');
   const temperatureElem = document.getElementById('temperature');
   const tempSymbolElem = document.getElementById('temp-symbol');
   const weatherDescElem = document.getElementById('weather-description');
   const humidityElem = document.getElementById('humidity');
   const windSpeedElem = document.getElementById('wind-speed');
 
-  cityNameElem.innerText = cityName;
   countryFlagImg.src = `https://www.countryflags.io/${countryCode}/shiny/24.png`;
+  cityNameElem.innerText = cityName;
+  localDateTimeElem.innerText = localDateTime;
   temperatureElem.innerText = Math.round(temperature);
   tempSymbolElem.innerHTML = tempSymbol;
   weatherDescElem.innerText =
     weatherDesc.charAt(0).toUpperCase() + weatherDesc.slice(1);
   humidityElem.innerText = `Humidity levels at: ${humidity}%`;
   windSpeedElem.innerText = `Winds at: ${Math.round(windSpeed)} ${speedUnit}`;
+
+  console.log(currentData);
 }
 
 async function showForecastData() {
@@ -81,27 +107,14 @@ async function showForecastData() {
     return element.dt_txt;
   });
 
-  let timeZoneOffset = forecastData.city.timezone / 3600;
-
-  if (timeZoneOffset > 0) timeZoneOffset = `+${timeZoneOffset}`;
-
-  const localDate = forecastData.list.map((element) => {
-    const date = DateTime.fromSeconds(element.dt, {
-      zone: `UTC${timeZoneOffset}`,
-    });
-    return date.toLocaleString(DateTime.DATETIME_MED);
-  });
+  const localDate = forecastData.list.map((element) =>
+    returnDateTime(element.dt, forecastData.city.timezone)
+  );
 
   console.log(forecastData);
   console.log(dt);
   console.log(localDate);
 }
-
-searchButton.addEventListener('click', () => {
-  setUnits();
-  showCurrentData();
-  showForecastData();
-});
 
 searchInput.addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
@@ -109,4 +122,10 @@ searchInput.addEventListener('keyup', (event) => {
     showCurrentData();
     showForecastData();
   }
+});
+
+searchButton.addEventListener('click', () => {
+  setUnits();
+  showCurrentData();
+  showForecastData();
 });
